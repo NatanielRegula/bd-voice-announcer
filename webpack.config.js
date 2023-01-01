7945eec3fe6577b0300e44580931558a175f3e96
nataniel regula <nataniel.regula17@gmail.com>
2023-01-01 21:13:45 +0000

    removed webpack config

--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  mode: 'production',
-  entry: './node_modules/uuid/dist/commonjs-browser/index.js',
-  output: {
-    path: __dirname + '/src/FemaleUKVoiceForVA',
-    filename: 'dependencies.js',
-  },
-};
